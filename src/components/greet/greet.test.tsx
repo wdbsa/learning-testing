769cf9c083a5e5563df72eb40c138b8a2edb8ea6
Mark Abuyabor <mark@zeguestlist.com>
2022-11-04 22:43:08 +0800

    adding describe

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,14 +1,20 @@
 import { render, screen } from "@testing-library/react"
 import Greet from "./greet"
 
-test('Greet renders correctly', () => {
-    render(<Greet />);
-    const textElement = screen.getByText("Hello");
-    expect(textElement).toBeInTheDocument();
+
+describe('Greet', () => {
+    test('renders correctly', () => {
+        render(<Greet />);
+        const textElement = screen.getByText("Hello");
+        expect(textElement).toBeInTheDocument();
+    })
 })
 
-test('Greet renders with a name', () => {
-    render(<Greet name='Mark' />);
-    const textElement = screen.getByText("Hello Mark");
-    expect(textElement).toBeInTheDocument();
+describe('Nested', () => {
+    test('renders with a name', () => {
+        render(<Greet name='Mark' />);
+        const textElement = screen.getByText("Hello Mark");
+        expect(textElement).toBeInTheDocument();
+    })
 })
+
