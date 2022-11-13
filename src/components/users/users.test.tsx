fe4c8a93ad791f662fc623de0577f8ecd51631e4
Mark Abuyabor <mark@zeguestlist.com>
2022-11-13 13:51:35 +0800

    msw testing

--- src/components/users/users.test.tsx
+++ src/components/users/users.test.tsx
@@ -1,6 +1,30 @@
 import {screen, render } from '@testing-library/react';
 import Users from './users';
+import { server } from '../../mocks/server'
+import { rest } from 'msw';
 
 describe('Users', () => {
-    
+    test("renders correctly", () => {
+        render(<Users />)
+        const textElement = screen.getByText("Users");
+        expect(textElement).toBeInTheDocument();
+    })
+
+    test('renders a list of users', async () => {
+        render(<Users />);
+        const userList = await screen.findAllByRole('listitem');
+        expect(userList).toHaveLength(1);
+    })
+
+    test('renders error', async () => {
+        server.use(
+            rest.get('https://jsonplaceholder.typicode.com/users', (req, res, ctx) => {
+                return res(ctx.status(500))
+            })
+        )
+        render(<Users />)
+        const error = await screen.findByText('Error fetching Users');
+        expect(error).toBeInTheDocument();
+
+    })
 })
